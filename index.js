35b919bb8b0c678c1b4e44b7ddd2b3cdfb55b14f
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-08 16:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random() * 20;
 
 const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = 1000000000 + randomInteger;
